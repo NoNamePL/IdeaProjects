312358167bb57d0e3703472cee557453a34d8d62
Roman <rdymshakov@inbox.ru>
2024-09-20 14:23:14 +0300

    in circle fill lineChart, and change image by push the button

--- WeatherWidget/src/main/java/saint/org/weatherwidget/weatherwidget/HelloController.java
+++ WeatherWidget/src/main/java/saint/org/weatherwidget/weatherwidget/HelloController.java
@@ -7,6 +7,8 @@ import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.chart.XYChart.Series;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import saint.org.weatherwidget.weatherwidget.ParseWeather.ParseWeather;
 import saint.org.weatherwidget.weatherwidget.WeatherDate.WeatherDate;
@@ -14,10 +16,13 @@ import saint.org.weatherwidget.weatherwidget.WeatherDate.WeatherDate;
 
 public class HelloController {
     @FXML
-    public Label temperature;
+    public Label currentTempLab;
 
     @FXML
     public LineChart<String,Number> lineChart;
+    @FXML
+
+    public ImageView weatherImage;
     @FXML
     private CategoryAxis xAxis;
     @FXML
@@ -31,26 +36,57 @@ public class HelloController {
     protected void onHelloButtonClick() {
 
         double temp = 0;
-        try {
-            ParseWeather parse = new ParseWeather();
-            WeatherDate weather = parse.getWeather();
-            temp = weather.getList().get(0).getMain().getTemp() - 273.15;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        String time = "";
+        ParseWeather parse = null;
 
-
+        // clear lineChart
         lineChart.getData().clear();
 
         welcomeText.setText("Welcome to JavaFX Application!");
 
-        XYChart.Series<String, Number> series = new XYChart.Series<String, Number>();
-        series.getData().add(new XYChart.Data<String, Number>("00:00", temp));
-        series.getData().add(new XYChart.Data<String, Number>("01:00", 17));
-        series.getData().add(new XYChart.Data<String, Number>("02:00", 20));
-        series.getData().add(new XYChart.Data<String, Number>("03:00", 25));
-        series.getData().add(new XYChart.Data<String, Number>("04:00", 30));
-        series.getData().add(new XYChart.Data<String, Number>("05:00", 24));
+        // create list of points for lineChart
+        Series<String, Number> series = new Series<String, Number>();
+
+        // pare date from open weather
+
+        try {
+            parse = new ParseWeather();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // get temperature from api and add it to lineChart data
+        WeatherDate weather = parse.getWeather();
+        for(int i = 0;i < 10;i++) {
+            temp = weather.getList().get(i).getMain().getTemp_min() - 273.15;
+            time = weather.getList().get(i).getDt_txt();
+
+            time = time.substring(time.indexOf(' '));
+
+            series.getData().add(new XYChart.Data<String, Number>(time, temp));
+        }
+
+
+        // set current temp into the label
+        double currentTemp = weather.getList().getFirst().getMain().getTemp_min() - 273.15;
+        currentTempLab.setText(String.valueOf(currentTemp));
+
+
+        // set image to weatherImage
+        try {
+            Image img = new Image(getClass().getResourceAsStream("/img/cloudy.png"));
+            weatherImage.setImage(img);
+        } catch (Exception e) {
+            System.out.println("Error is here");
+            e.printStackTrace();
+        }
+
+
+//        series.getData().add(new XYChart.Data<String, Number>("00:00", temp));
+//        series.getData().add(new XYChart.Data<String, Number>("01:00", 17));
+//        series.getData().add(new XYChart.Data<String, Number>("02:00", 20));
+//        series.getData().add(new XYChart.Data<String, Number>("03:00", 25));
+//        series.getData().add(new XYChart.Data<String, Number>("04:00", 30));
+//        series.getData().add(new XYChart.Data<String, Number>("05:00", 24));
 
         series.setName("Number of temperatures");
 
